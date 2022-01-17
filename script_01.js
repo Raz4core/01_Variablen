bc9d29890bbc674f84675a4afe653d6bc09f4455
Raz4core <mail@test.de>
2022-01-17 10:28:56 +0100

    let Test

--- script_01.js
+++ script_01.js
@@ -7,7 +7,6 @@
  * Block-KOmmentar
  *
  */
-
 // Zeilen-Kommentar
 
 // Konsolen-Ausgabe
@@ -16,10 +15,28 @@
 
 /****02 Deklaration + Wertzuweisung ****/
 
-let firstName; //Deklaration (Definition)
-firstName = "Max"; // Wertzuweisung
-console.log(firstName); // Ausgabe
+// let firstName; //Deklaration (Definition)
+//firstName = "Max"; // Wertzuweisung
+//console.log(firstName); // Ausgabe
 
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
-console.log(familyName);
-console.log(firstName +" "+ familyName);
+//let familyName = "Mütze"; // Deklaration + Wertzuweisung
+//console.log(familyName);
+//console.log(firstName +" "+ familyName);
+
+/**03 deklaration + Wertzustellung II */
+//let firstName,familyName;
+//firstName = prompt("Bitte Vornamen eingeben");
+//familyName = prompt("Bitte Nachnamen eingeben");
+//console.log(firstName + " " + familyName);
+
+
+
+// JS ist eine untypisierte Sprache! untyped
+let test;
+test = "Hi";
+test = 2;
+test = true;
+
+console.log ("Datentyp:" + typeof test);
+console.log ("Inhalt:" + test);
+
